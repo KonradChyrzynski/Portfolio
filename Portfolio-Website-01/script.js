24858f0ba8651759affd5d266aa16cbb686502b8
Konrad Chyrzynski <konrad.chyrzynskii@gmail.com>
2021-08-31 18:17:28 +0200

    Make navbar responsive on small screan (PortfolioWebsite01)

--- Portfolio-Website-01/script.js
+++ Portfolio-Website-01/script.js
@@ -1,12 +1,16 @@
 const nav = document.querySelector("nav")
 const hamburger = document.querySelector(".hamburger")
+const navItemsMid = document.querySelector(".nav-items-mid");
+const navItemsRight = document.querySelector(".nav-items-right");
+const navItemLeft = document.querySelector(".nav-item-left");
 
 nav.appendChild(hamburger)
 
 
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
+    navItemsMid.classList.toggle("nav-open");
+    navItemsRight.classList.toggle("nav-open")
+    navItemLeft.classList.toggle("spense-margin")
     nav.classList.toggle("nav-small-screan");
-    linesContainer.classList.toggle("lines-container-open");
 })
